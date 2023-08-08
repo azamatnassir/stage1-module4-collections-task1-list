0d119ecb2a5a11fe95148443e670dd0b9712f471
azamatnassir <azamatnsr@gmail.com>
2023-08-08 12:52:44 +0600

    commit

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0)
+                linkedList.addLast(num);
+            else
+                linkedList.addFirst(num);
+        }
+        return linkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int x1 = (int) (5 * Math.pow(Integer.parseInt(a), 2) + 3);
+        int x2 = (int) (5 * Math.pow(Integer.parseInt(b), 2) + 3);
+
+        if (x1 == x2)
+            return Integer.compare(Integer.parseInt(a), Integer.parseInt(b));
+        else if (x1 > x2)
+            return 1;
+        else
+            return -1;
     }
 }
